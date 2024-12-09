2b03272a4fe27e8418611ccd0bf7bdd8fe645fea
ralucagog <ralugog83@yahoo.com>
2024-12-09 12:05:25 +0200

    Vacation request - Backend implementation

--- backend/src/main/java/com/unternehmensplattform/backend/services/implementations/WorkingDaysServiceImpl.java
+++ backend/src/main/java/com/unternehmensplattform/backend/services/implementations/WorkingDaysServiceImpl.java
@@ -1,12 +1,51 @@
 package com.unternehmensplattform.backend.services.implementations;
 
+import com.unternehmensplattform.backend.entities.Contract;
+import com.unternehmensplattform.backend.entities.User;
+import com.unternehmensplattform.backend.entities.VacationRequest;
+import com.unternehmensplattform.backend.entities.WorkingDay;
+import com.unternehmensplattform.backend.enums.UserRole;
+import com.unternehmensplattform.backend.handler.VacationRequestNotFoundException;
 import com.unternehmensplattform.backend.repositories.WorkingDaysRepository;
 import com.unternehmensplattform.backend.services.interfaces.WorkingDaysService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class WorkingDaysServiceImpl implements WorkingDaysService {
     private final WorkingDaysRepository workingDaysRepository;
+
+    @Override
+    public void deleteWorkingDay(Integer requestId) {
+        WorkingDay workingDay = workingDaysRepository .findById(requestId).orElse(null);
+        if (workingDay == null) {
+            throw new VacationRequestNotFoundException("Working day with id " + requestId + " not found.");
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        if (currentUser.getRole() == UserRole.Employee || currentUser.getRole()==UserRole.Administrator) {
+            Contract contract = currentUser.getContract();
+            if (contract != null) {
+                if (Objects.equals(workingDay.getEmployee().getId(), currentUser.getId())) {
+//                    contract.setActualYearVacationDays(contract.getActualYearVacationDays() + (int) calculateWeekdays(vacationRequest.getStartDate(), vacationRequest.getEndDate()));
+//                    contractRepository.save(contract);
+
+
+                    ///acolo ar trb logica de scazut 8 ore
+                    workingDaysRepository.deleteById(requestId);
+                } else {
+                    throw new IllegalArgumentException("working day does not belong to the employee who made the request");
+                }
+            } else {
+                throw new RuntimeException("Employee has no contract");
+            }
+        } else {
+            throw new RuntimeException("User is not an employee or an admin");
+        }
+    }
 }
